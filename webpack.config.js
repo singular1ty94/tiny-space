7c1b54e452972f36a7b7c388cafa673460f7790f
Brett <singular1ty@outlook.com>
2017-11-24 10:55:15 +1000

    Moved to SCSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,12 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractSass = new ExtractTextPlugin({
+  filename: "main.bundle.css"
+});
+
 module.exports = {
-  entry: path.join(__dirname, '', 'index'),
+  entry: [path.join(__dirname, '', 'index'), path.join(__dirname, '', 'main.scss')],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
@@ -19,8 +25,23 @@ module.exports = {
       query: {
         presets: ['es2015', 'react', 'stage-3']
       }
-    }]
+    },{
+      test: /\.scss$/,
+      use: extractSass.extract({
+          use: [{
+              loader: "css-loader"
+          }, {
+              loader: "sass-loader"
+          }],
+          // use style-loader in development
+          fallback: "style-loader"
+      })
+    },
+    { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader' }]
   },
+  plugins: [
+    extractSass
+  ],
   resolve: {
     extensions: ['.json', '.js', '.jsx', '.css']
   },
